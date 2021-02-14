05a7571268da6137394067a5eebef1d95ea94f0a
Mikhail Ilyushin <ilyushin_misha@mail.ru>
2021-02-14 18:40:56 +0300

    Directory hell solved. Add web project for presentation layer for clients

---
+++ src/Web/FoundersPC.Web/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using FoundersPC.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using FoundersPC.Application;
+
+namespace FoundersPC.Web.Controllers
+{
+    public sealed class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> ApiResult()
+        {
+            var response = await GlobalContext.WeClient.GetAsync($"cpus/1");
+            var result = await response.Content.ReadFromJsonAsync<CaseReadDto>();
+
+            return View(result);
+        }
+    }
+}
---
+++ src/Web/FoundersPC.Web/GlobalContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FoundersPC.Web
+{
+    public class GlobalContext
+    {
+        public static readonly HttpClient WeClient = new();
+
+        static GlobalContext()
+        {
+            WeClient.BaseAddress = new Uri("https://localhost:5001/api/");
+            WeClient.DefaultRequestHeaders.Clear();
+            WeClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            WeClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8");
+        }
+    }
+}
