d6d21422a7dcca180452ae5a44f4ca4252576e33
Mikhail Ilyushin <ilyushinmisha@yandex.ru>
2022-01-04 22:50:05 +0300

    - Add necessary mappings

--- src/FoundersPC.Application/Mappings/HardwareMappingProfile.cs
+++ src/FoundersPC.Application/Mappings/HardwareMappingProfile.cs
@@ -1,6 +1,14 @@
 ﻿using AutoMapper;
 using FoundersPC.Application.Features.Hardware;
+using FoundersPC.Application.Features.Hardware.Case.Models;
+using FoundersPC.Application.Features.Hardware.HardDriveDisk.Models;
 using FoundersPC.Application.Features.Hardware.Models;
+using FoundersPC.Application.Features.Hardware.Motherboard.Models;
+using FoundersPC.Application.Features.Hardware.PowerSupply.Models;
+using FoundersPC.Application.Features.Hardware.Processor.Models;
+using FoundersPC.Application.Features.Hardware.RandomAccessMemory.Models;
+using FoundersPC.Application.Features.Hardware.SolidStateDrive.Models;
+using FoundersPC.Application.Features.Hardware.VideoCard.Models;
 using FoundersPC.Domain.Entities.Hardware;
 using FoundersPC.Domain.Entities.Hardware.Metadata;
 
@@ -31,5 +39,45 @@ public class HardwareMappingProfile : Profile
         CreateMap<Producer, ProducerInfo>()
             .ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.CreatedBy.Login))
             .ForMember(dest => dest.LastModifiedBy, opt => opt.MapFrom(src => src.LastModifiedBy.Login));
+
+        CreateMap<Case, CaseInfo>()
+            .IncludeMembers(x => x.Metadata);
+
+        CreateMap<CaseMetadata, CaseInfo>();
+
+        CreateMap<HardDriveDisk, HardDriveDiskInfo>()
+            .IncludeMembers(x => x.Metadata);
+
+        CreateMap<HardDriveDiskMetadata, HardDriveDiskInfo>();
+
+        CreateMap<Motherboard, MotherboardInfo>()
+            .IncludeMembers(x => x.Metadata);
+
+        CreateMap<MotherboardMetadata, MotherboardInfo>();
+
+        CreateMap<PowerSupply, PowerSupplyInfo>()
+            .IncludeMembers(x => x.Metadata);
+
+        CreateMap<PowerSupplyMetadata, PowerSupplyInfo>();
+
+        CreateMap<Processor, ProcessorInfo>()
+            .IncludeMembers(x => x.Metadata);
+
+        CreateMap<ProcessorMetadata, ProcessorInfo>();
+
+        CreateMap<RandomAccessMemory, RandomAccessMemoryInfo>()
+            .IncludeMembers(x => x.Metadata);
+
+        CreateMap<RandomAccessMemoryMetadata, RandomAccessMemoryInfo>();
+
+        CreateMap<SolidStateDrive, SolidStateDriveInfo>()
+            .IncludeMembers(x => x.Metadata);
+
+        CreateMap<SolidStateDriveMetadata, SolidStateDriveInfo>();
+
+        CreateMap<VideoCard, VideoCardInfo>()
+            .IncludeMembers(x => x.Metadata);
+
+        CreateMap<VideoCardMetadata, VideoCardInfo>();
     }
 }
--- src/FoundersPC.Application/Mappings/RequestToQueryMappingProfile.cs
+++ src/FoundersPC.Application/Mappings/RequestToQueryMappingProfile.cs
@@ -10,25 +10,73 @@ public class RequestToQueryMappingProfile : Profile
         CreateMap<Features.Hardware.Case.GetRequest, Features.Hardware.Case.GetQuery>()
             .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.Case));
 
+        CreateMap<Features.Hardware.Case.UpdateRequest, Features.Hardware.Case.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.Case));
+
+        CreateMap<Features.Hardware.Case.DeleteRequest, Features.Hardware.Case.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.Case));
+
         CreateMap<Features.Hardware.HardDriveDisk.GetRequest, Features.Hardware.HardDriveDisk.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.HDD));
+
+        CreateMap<Features.Hardware.HardDriveDisk.UpdateRequest, Features.Hardware.HardDriveDisk.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.HDD));
+
+        CreateMap<Features.Hardware.HardDriveDisk.DeleteRequest, Features.Hardware.HardDriveDisk.GetQuery>()
             .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.HDD));
 
         CreateMap<Features.Hardware.Motherboard.GetRequest, Features.Hardware.Motherboard.GetQuery>()
             .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.MB));
 
+        CreateMap<Features.Hardware.Motherboard.UpdateRequest, Features.Hardware.Motherboard.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.MB));
+
+        CreateMap<Features.Hardware.Motherboard.DeleteRequest, Features.Hardware.Motherboard.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.MB));
+
         CreateMap<Features.Hardware.PowerSupply.GetRequest, Features.Hardware.PowerSupply.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.FPU));
+
+        CreateMap<Features.Hardware.PowerSupply.UpdateRequest, Features.Hardware.PowerSupply.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.FPU));
+
+        CreateMap<Features.Hardware.PowerSupply.DeleteRequest, Features.Hardware.PowerSupply.GetQuery>()
             .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.FPU));
 
         CreateMap<Features.Hardware.Processor.GetRequest, Features.Hardware.Processor.GetQuery>()
             .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.CPU));
 
+        CreateMap<Features.Hardware.Processor.UpdateRequest, Features.Hardware.Processor.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.CPU));
+
+        CreateMap<Features.Hardware.Processor.DeleteRequest, Features.Hardware.Processor.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.CPU));
+
         CreateMap<Features.Hardware.RandomAccessMemory.GetRequest, Features.Hardware.RandomAccessMemory.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.RAM));
+
+        CreateMap<Features.Hardware.RandomAccessMemory.UpdateRequest, Features.Hardware.RandomAccessMemory.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.RAM));
+
+        CreateMap<Features.Hardware.RandomAccessMemory.DeleteRequest, Features.Hardware.RandomAccessMemory.GetQuery>()
             .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.RAM));
 
         CreateMap<Features.Hardware.SolidStateDrive.GetRequest, Features.Hardware.SolidStateDrive.GetQuery>()
             .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.SSD));
 
+        CreateMap<Features.Hardware.SolidStateDrive.UpdateRequest, Features.Hardware.SolidStateDrive.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.SSD));
+
+        CreateMap<Features.Hardware.SolidStateDrive.DeleteRequest, Features.Hardware.SolidStateDrive.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.SSD));
+
         CreateMap<Features.Hardware.VideoCard.GetRequest, Features.Hardware.VideoCard.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.GPU));
+
+        CreateMap<Features.Hardware.VideoCard.UpdateRequest, Features.Hardware.VideoCard.GetQuery>()
+            .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.GPU));
+
+        CreateMap<Features.Hardware.VideoCard.DeleteRequest, Features.Hardware.VideoCard.GetQuery>()
             .ForMember(dest => dest.HardwareTypeId, opt => opt.MapFrom(_ => (int)HardwareType.GPU));
     }
 }
